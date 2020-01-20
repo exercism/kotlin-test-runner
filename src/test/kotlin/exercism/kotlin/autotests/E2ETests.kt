451d5cc4b2eeedf5433957219d69db11c6161d40
Denys M <dector9@gmail.com>
2020-01-20 11:18:23 +0400

    Fix stack trace readability for E2E tests

--- src/test/kotlin/exercism/kotlin/autotests/E2ETests.kt
+++ src/test/kotlin/exercism/kotlin/autotests/E2ETests.kt
@@ -1,39 +1,38 @@
 package exercism.kotlin.autotests
 
 import exercism.kotlin.autotests.runner.main
+import io.kotlintest.TestContext
 import io.kotlintest.shouldBe
-import io.kotlintest.specs.AbstractStringSpec
 import io.kotlintest.specs.StringSpec
 import java.io.File
 import java.nio.file.Files
 
 class E2ETests : StringSpec({
-    testCase("single passing test", "1")
-    testCase("compilation error", "2")
-    testCase("some tests are failing", "3")
+    "single passing test"(testCase("1"))
+    "compilation error"(testCase("2"))
+    "some tests are failing"(testCase("3"))
+
     // TODO add more tests with other cases
 })
 
-private fun AbstractStringSpec.testCase(name: String, id: String) {
-    name {
-        // Prepare working dir
-        val workingDir = Files.createTempDirectory("exercism-kotlin-test-runner").toFile()
+private fun testCase(id: String): suspend TestContext.() -> Unit = {
+    // Prepare working dir
+    val workingDir = Files.createTempDirectory("exercism-kotlin-test-runner").toFile()
 
-        try {
-            File("examples/template").copyRecursively(workingDir)
-            File("examples/$id").copyRecursively(workingDir)
+    try {
+        File("examples/template").copyRecursively(workingDir)
+        File("examples/$id").copyRecursively(workingDir)
 
-            // Execute program
-            val outputDir = workingDir.resolve("build/")
-            main(arrayOf("exercise-slug", workingDir.absolutePath, outputDir.absolutePath))
+        // Execute program
+        val outputDir = workingDir.resolve("build/")
+        main(arrayOf("exercise-slug", workingDir.absolutePath, outputDir.absolutePath))
 
-            // Assert result
-            val actual = outputDir.resolve("results.json").readText().trim()
-            val expected = workingDir.resolve("expected_results.json").readText().trim()
+        // Assert result
+        val actual = outputDir.resolve("results.json").readText().trim()
+        val expected = workingDir.resolve("expected_results.json").readText().trim()
 
-            actual shouldBe expected
-        } finally {
-            workingDir.deleteRecursively()
-        }
+        actual shouldBe expected
+    } finally {
+        workingDir.deleteRecursively()
     }
 }
