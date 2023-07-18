9d9f62ac9d2d89e4fe689a99e3349ac5c9438ef2
Erik Schierboom <erik_schierboom@hotmail.com>
2023-07-18 10:36:54 +0200

    Upgrade to Kotlin 1.9.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 
 plugins {
-    kotlin("jvm") version "1.8.22"
+    kotlin("jvm") version "1.9.0"
     id("com.github.johnrengelman.shadow") version "8.1.1"
     application
 }
